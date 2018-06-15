2c0590430520ddf0cb82ad38d12cd414f1595bd6
Zhiqiu Hu <zhiqiu.hu@gmail.com>
2018-06-15 13:21:16 +0000

    version 1.5.1

--- R/distfree.cr.R
+++ R/distfree.cr.R
@@ -1,17 +1,56 @@
 #get the cross points of lines a=slop, b=intercept
-linescrosspoint<-function(a, b){
-  b1=b[-NROW(b)]
-  b2=b[-1]
-  a1=a[-NROW(a)]
-  a2=a[-1]
+linescrosspoint<-function(a, b, a2=NA, b2=NA){
+  if (missing(b2)){
+    b1=b[-NROW(b)]
+    b2=b[-1]
+  } else b1=b;
+  if (missing (a2)){
+    a1=a[-NROW(a)]
+    a2=a[-1]
+  } else {a1=a}
   
   x1=-(b1-b2)/(a1-a2)
   y1=-(a2*b1-a1*b2)/(a1-a2)
   na.exclude(data.frame(x=x1, y=y1))
 }
 
+AngleToSlope<-function(x) {
+  if (cos(x)==0) res=1e50 else res=sin(x)/cos(x)
+  res
+}
+
+linescrosspoint.plus<-function(lines){
+  p0=(colMeans(linescrosspoint(a=c(lines[,1], lines[1:2,1]), b=c(lines[,2], lines[1:2,2]))))
+  p0=data.frame(x=p0[1], y=p0[2])
+  cross.distance=(lines$b*p0$x-p0$y+lines$incpt)/sqrt(lines$b^2+1)
+  
+  idx=which(abs(cross.distance)==min(abs(cross.distance)))
+  line.idx=idx
+  
+  p1=linescrosspoint(c(lines$b[idx], AngleToSlope(pi/2+lines$angle[idx])),
+                     c(lines$incpt[idx], -AngleToSlope(pi/2+lines$angle[idx])*p0$x+p0$y))
+  while (1==1) {
+    sub.line.idx=(1:((NROW(lines))/4))+line.idx[NROW(line.idx)]
+    sub.line.idx[which(sub.line.idx>NROW(lines))]=sub.line.idx[which(sub.line.idx>NROW(lines))]-NROW(lines)
+    sub.line=lines[sub.line.idx,]
+    cur.line=lines[line.idx[NROW(line.idx)],]
+    xy=linescrosspoint(sub.line$b, sub.line$incpt, cur.line$b, cur.line$incpt)
+    cross.distance=(xy$x-p1$x)^2+(xy$y-p1$y)^2
+    idx=which(abs(cross.distance)==min(abs(cross.distance)))
+    idx=idx[NROW(idx)]
+    p1=xy[idx,]
+    line.idx=c(line.idx, sub.line.idx[idx])
+    if (line.idx[NROW(line.idx)]==line.idx[1]) break
+  }
+  
+  linescrosspoint(lines$b[line.idx], lines$incpt[line.idx])
+}
+
+
+
 #get range quantile for scatter of x, y
 xy.quantile<-function(x, y, alpha=0.05, half.nknots=90) { 
+  
   probs=range(c(alpha/2, 1-alpha/2))
   anglelist=seq(-pi/2, pi/2-pi*(0.5/half.nknots), length.out=half.nknots+1)
   res=c(1)[0]
@@ -30,13 +69,15 @@ xy.quantile<-function(x, y, alpha=0.05, half.nknots=90) {
   
   
   lines=data.frame(b=c(res[,1],res[,1]), incpt=c(res[,2],res[,3]), angle=c(res[,4], res[,4]+pi)%%(2*pi), c=c(res[,5], res[,5]))
+#   p=linescrosspoint(a=c(lines[,1], lines[1:2,1]), b=c(lines[,2], lines[1:2,2]))
+  p=linescrosspoint.plus(lines)
   
-  p=linescrosspoint(a=c(lines[,1], lines[1:2,1]), b=c(lines[,2], lines[1:2,2]))
+  
   list(polygon=p, data=data.frame(x=x, y=y, pip=1-off.points), alpha.realized=sum(off.points)/NROW(x))
 }
 
 plot.distfree.cr<-function(x, show.points=T, ...) {
-obj=x;
+  obj=x;
   xlim=range(c(obj$data[,1], obj$polygon$x))
   ylim=range(c(obj$data[,2], obj$polygon$y))
   plot(NA, type="n", xlim=xlim, ylim=ylim, xlab=obj$xlab, ylab=obj$ylab)
@@ -44,15 +85,19 @@ obj=x;
     points(obj$data, col=obj$col[2:3][2-obj$data$pip])
   }
   polygon(obj$polygon, border=obj$col[1], lwd=2.0)
-  polygon(obj$polygon.smooth1, border=obj$col[1], lwd=2.0, lty="dotted")
-  polygon(obj$polygon.smooth2, border=obj$col[1], lwd=2.0,  lty="dashed")
-  legend("topright", legend=c("Apex connection", "Smooth1", "Smooth2"), col=obj$col[1], lty=c("solid", "dotted", "dashed"), bty="n")
+  legend("topright",lty=c("solid", "blank", "blank"), bty="n",
+         legend=c(paste("Confidence region (p=", 1-x$alpha.realized, ")", sep=""),
+                  "PIP (points in polygon)", "non-PIP"),
+         pch=c(NA, 1, 1),
+         col=c("red", "black", "gray")
+  )
 }
 
-distfree.cr<-function(x, y, alpha=0.05, alpha.min.diff=alpha/10, nknots=40,
+distfree.cr<-function(x, y, alpha=0.05, alpha.min.diff=0.5/NROW(x), nknots=40,
                       xlab = deparse(substitute(x)), 
                       ylab = deparse(substitute(y)),
                       col=c("red", "black", "gray"), draw=T) {
+
   if (missing(y)) {
     if (NCOL(x) == 2) {
       if (missing(xlab)) xlab <- colnames(x)[1]
@@ -61,41 +106,44 @@ distfree.cr<-function(x, y, alpha=0.05, alpha.min.diff=alpha/10, nknots=40,
       x <- x[, 1]
     } else stop("x and y must be vectors, or x must be a 2 column matrix")
   } else if (!(is.vector(x) && is.vector(y) && length(x) == length(y))) stop("x and y must be vectors of the same length")
-  col=rep(col, 3)
+  col=rep(col, 3)[1:3]
+  
+#   if ((NROW(x)*alpha)<2) {
+#     xy.quant=(list(polygon=border.points.idx(data.frame(x=x, y=y)), data=data.frame(x=x, y=y, pip=1), alpha.realized=0))
+#     xy.quant$alpha=alpha
+#     xy.quant$xlab=xlab
+#     xy.quant$ylab=ylab
+#     xy.quant$col=col[1:3]
+#     xy.quant$polygon.smooth1=xy.quant$polygon
+#     xy.quant$polygon.smooth2=xy.quant$polygon
+#     return (xy.quant)
+#   }
+  
+  nknots=round(nknots/2)*2
   
   est.alpha=alpha/nknots
-  diff=0.5
-  while (diff>alpha.min.diff) {
+  min.diff=1
+  min.diff.est.alpha=est.alpha
+  for (iter in 1:40) {
     xy.quant=xy.quantile(x=x, y=y, half.nknots=round(nknots/2), alpha=est.alpha)
-    diff=abs(alpha-xy.quant$alpha.realized)
-    est.alpha=est.alpha*alpha/xy.quant$alpha.realized
+#     cat("input=",est.alpha, "  realized=",xy.quant$alpha.realized, "  Target=",alpha, "\n")
+    if (abs(alpha-xy.quant$alpha.realized)<=alpha.min.diff) break;
+    if (min.diff>=abs(alpha-xy.quant$alpha.realized)) {
+      min.diff=abs(alpha-xy.quant$alpha.realized)
+      min.diff.est.alpha=est.alpha;
+    } else {
+      xy.quant=xy.quantile(x=x, y=y, half.nknots=round(nknots/2), alpha=min.diff.est.alpha)
+      break
+    }
+#     est.alpha=min(max(0, est.alpha*alpha/(1e-200+xy.quant$alpha.realized)), 1);
+     est.alpha=min(max(0, est.alpha/max(min(xy.quant$alpha.realized/alpha, 100), 0.01)), 1);
+    
   }
+
   xy.quant$alpha=alpha
   xy.quant$xlab=xlab
   xy.quant$ylab=ylab
   xy.quant$col=col[1:3]
-  
-  #   #smooth the regions
-  #   for (k in 2:(NROW(xy.quant$polygon)-1)) {
-  #     if (sum((xy.quant$polygon[k-1,]-xy.quant$polygon[k,])^2)>sum((xy.quant$polygon[k-1,]-xy.quant$polygon[k+1,])^2)) {
-  #       xy.quant$polygon[c(k, k+1),]=xy.quant$polygon[c(k+1, k),]  
-  #     }
-  #   }  
-  
-  xy.quant$polygon.smooth1=xy.quant$polygon;
-  for (k in 2:(NROW(xy.quant$polygon.smooth1)-1)) {
-    if (sum((xy.quant$polygon.smooth1[k-1,]-xy.quant$polygon.smooth1[k,])^2)>sum((xy.quant$polygon.smooth1[k-1,]-xy.quant$polygon.smooth1[k+1,])^2)) {
-      xy.quant$polygon.smooth1[c(k, k+1),]=xy.quant$polygon.smooth1[c(k+1, k),]  
-    }
-  }
-  
-  
-
-  idx=which(xy.quant$data$pip==1)
-  tmp=xy.quant$data[,1:2][idx,];
-  idx=border.points.idx(tmp)
-  
-  xy.quant$polygon.smooth2=tmp[idx,]
   
   class(xy.quant)<-"distfree.cr"
   if (draw) plot(xy.quant)
@@ -115,26 +163,24 @@ xytoangle<-function(x, y) {
 #get index of outer points in x=data.frame(row, col)
 border.points.idx<-function(x) {
   x=data.frame(row=x[,1], col=x[,2])
-  
+
   k=order(x$row)[1]
   bord.points=k;
-  ang0=pi/2;
+  ang0=pi;
   
   while(NROW(bord.points)==NROW(unique(bord.points))) { 
     ang=xytoangle(x=x$row-x$row[k], y=x$col-x$col[k])
+    len=(x$row-x$row[k])^2+(x$col-x$col[k])^2
     ang_diff=ang-ang0
     idx=which(ang_diff>pi,arr.ind=T) 
     ang_diff=ang_diff%%(2*pi)
-    
     minang=min(ang_diff,  na.rm = T)
-    k=which(minang==(ang_diff), arr.ind=T)
+    k=which((minang==(ang_diff))&(len>0), arr.ind=T)
+    k=k[which(len[k]==max(len[k]))[1]]
+
     bord.points=c(bord.points, k)
-    ang0=ang[k]%%(2*pi)
+    ang0=ang[k][1]%%(2*pi)
   }
   bord.points
 }
-
-# dat=pt$dat[which(pt$dat[,3]==1),][,1:2]
-# idx=border.points.idx(dat)
-# polygon(dat[idx,][,1:2])
 
